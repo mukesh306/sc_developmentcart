b3643fbdd3e8c73821208102b5a8374dc4c351d0
mukesh306 <mukesh@developmentcart.com>
2025-05-29 12:50:03 +0530

    update env
    env

--- config/db.js
+++ config/db.js
@@ -1,11 +1,28 @@
-const mongoose  = require('mongoose');
+// const mongoose  = require('mongoose');
+// const connectDB = async () => {
+//     try{
+//         await mongoose.connect(process.env.MONGODB_URI);
+//         console.log('MongoDB connected');
+//     } catch (err){
+// console.error('MongoDB connection error:', err.message);
+// process.exit(1);
+//     }
+// };
+// module.exports= connectDB;
+
+const mongoose = require('mongoose');
+
 const connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log('MongoDB connected');
-    } catch (err){
-console.error('MongoDB connection error:', err.message);
-process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('✅ MongoDB connected');
+  } catch (err) {
+    console.error('❌ MongoDB connection error:', err.message);
+    process.exit(1);
+  }
 };
-module.exports= connectDB;
+
+module.exports = connectDB;
