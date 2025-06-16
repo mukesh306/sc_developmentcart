219be211c1a90e59ad122c71d9b93e9147f6ab09
mukesh306 <mukesh@developmentcart.com>
2025-06-16 17:18:08 +0530

    update
    update

--- middleware/upload.js
+++ middleware/upload.js
@@ -7,7 +7,6 @@ const uploadPath = path.join(__dirname, '../uploads');
 if (!fs.existsSync(uploadPath)) {
   fs.mkdirSync(uploadPath);
 }
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, uploadPath),
   filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
