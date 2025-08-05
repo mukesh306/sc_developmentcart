2817f8bd4ceced690e4da64ddcae1b2db0f98df7
mukesh306 <mukesh@developmentcart.com>
2025-08-05 10:33:56 +0530

    update
    update

--- routes/practicesRoutes.js
+++ routes/practicesRoutes.js
@@ -8,6 +8,6 @@ const checkUserSession = require('../middleware/checkUserSession');
 router.post('/PracticeTest',auth,practiceController.PracticeTest);
 router.get('/getAssignedListUserpractice',auth,practiceController.getAssignedListUserpractice);
 router.post('/calculatePracticeScore',auth,practiceController.calculateQuizScoreByLearning);
-
+router.get('/StrikePath', auth, practiceController.userView);
 
 module.exports = router;
