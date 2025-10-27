69e7695879ec2ca4ca8386e250882ba6b92b7364
mukesh306 <mukesh@developmentcart.com>
2025-10-27 17:45:03 +0530

    update
    update examquestion

--- routes/SchoolerexamRoutes.js
+++ routes/SchoolerexamRoutes.js
@@ -7,10 +7,12 @@ router.post("/schooler/createExam", auth, SchoolerexamController.createExam);
 router.get("/schooler/getAllExams", SchoolerexamController.getAllExams);
 
 router.get("/schooler/getExam/:id", SchoolerexamController.getExamById);
+
 router.put("/schooler/updateExam/:id", auth, SchoolerexamController.updateExam);
 router.delete("/schooler/deleteExam/:id", auth, SchoolerexamController.deleteExam);
 
 router.get("/schooler/UsersExams",auth, SchoolerexamController.UsersExams);
+router.get("/schooler/ExamQuestion/:id", SchoolerexamController.ExamQuestion);
 router.post("/schooler/submitExamAnswer", auth, SchoolerexamController.submitExamAnswer);
 router.post("/schooler/calculateExamResult", auth, SchoolerexamController.calculateExamResult);
 router.post("/schooler/getTopUsersPerGroup", SchoolerexamController.getTopUsersPerGroup);
