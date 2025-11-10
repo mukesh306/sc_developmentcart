cc1b17c2439de1c2cefe4aff72127a781cfc5c2c
mukesh306 <mukesh@developmentcart.com>
2025-11-10 15:22:38 +0530

    update  delete
    deete

--- routes/userexamGroupRoutes.js
+++ routes/userexamGroupRoutes.js
@@ -4,7 +4,7 @@ const userExamGroupController = require("../controllers/userexamgroupController"
 router.post("/Schooler/usergroupCreate", userExamGroupController.createGroup);
 router.get("/Schooler/AlluserExamGroups", userExamGroupController.AlluserExamGroups);
 router.put("/Schooler/updateGroup/:groupId", userExamGroupController.updateGroup);
-router.delete("/Schooler/deleteGroup/:id", userExamGroupController.deleteGroup);
+router.delete("/Schooler/deleteGroup/:groupId", userExamGroupController.deleteGroup);
 router.get("/Schooler/getAllActiveUsers", userExamGroupController.getAllActiveUsers);
 
 module.exports = router;
