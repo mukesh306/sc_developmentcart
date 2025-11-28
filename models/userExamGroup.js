96939c81320943a24ce1c7668ef7e1e0e0a44b55
mukesh306 <mukesh@developmentcart.com>
2025-11-28 10:18:36 +0530

    update group created
    groupcreated

--- models/userExamGroup.js
+++ models/userExamGroup.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const userexamGroupSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   category:{ type: mongoose.Schema.Types.ObjectId, ref: "Schoolercategory" },
  className: { type: mongoose.Schema.Types.ObjectId, ref: 'Adminschool' },
   createdAt: { type: Date, default: Date.now },
 });
-
+userexamGroupSchema.index({ category: 1, name: 1 }, { unique: true })
 module.exports = mongoose.model("userexamGroup", userexamGroupSchema);
