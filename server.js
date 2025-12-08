a22eb286827bd75ec888d86022abed4194562b87
mukesh306 <mukesh@developmentcart.com>
2025-12-08 12:44:21 +0530

    update
    update

--- server.js
+++ server.js
@@ -278,43 +278,25 @@ setInterval(async () => {
           examObj.rank = null;
         }
 
-  else if (statusManage === "Completed") {
-  // previously saved result ko change nahi karna
-  examObj.result = result || status.result || null;
-
-  if (examFullyCompleted) {
-   
-    setTimeout(async () => {
-      examObj.rank = status.rank || null;
-      if (!status.rank) {
-        await calculateFinalRank(exam._id);
-        let updatedStatus = await ExamUserStatus.findById(status._id).lean();
-        examObj.rank = updatedStatus?.rank || null;
-      }
-      socket.emit("examStatusUpdate", [examObj]);
-    }, 5000); 
-  } else {
-    examObj.rank = null;
-  }
-}
+ 
 
 
 
-        // else if (statusManage === "Completed") {
+        else if (statusManage === "Completed") {
          
-        //   examObj.result = result || status.result || null;
+          examObj.result = result || status.result || null;
 
-        //   if (examFullyCompleted) {
-        //     examObj.rank = status.rank || null;
-        //     if (!status.rank) {
-        //       await calculateFinalRank(exam._id);
-        //       let updatedStatus = await ExamUserStatus.findById(status._id).lean();
-        //       examObj.rank = updatedStatus?.rank || null;
-        //     }
-        //   } else {
-        //     examObj.rank = null;
-        //   }
-        // }
+          if (examFullyCompleted) {
+            examObj.rank = status.rank || null;
+            if (!status.rank) {
+              await calculateFinalRank(exam._id);
+              let updatedStatus = await ExamUserStatus.findById(status._id).lean();
+              examObj.rank = updatedStatus?.rank || null;
+            }
+          } else {
+            examObj.rank = null;
+          }
+        }
 
         userExams.push(examObj);
       }
