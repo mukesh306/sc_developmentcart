7b47b23aa6322a84adb8e54c691f66e88289be31
mukesh306 <mukesh@developmentcart.com>
2026-01-05 16:08:14 +0530

    update
    update

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -1612,7 +1612,7 @@ exports.PracticescoreCard = async (req, res) => {
 
    
     // const otherScores = finalScores.filter(item => item.date !== todayStr);
-const otherScores = sortedFinal;
+const otherScores = finalScores;
     const validScores = finalScores.filter(s => s.score !== null);
     const avg =
       validScores.reduce((sum, s) => sum + s.score, 0) /
