1102fcf9fa7ff6637bf5da9ee2a14af555da736e
mukesh306 <mukesh@developmentcart.com>
2026-01-14 13:41:02 +0530

    update
    update

--- config/firebase.js
+++ config/firebase.js
@@ -1,25 +1,25 @@
-const admin = require("firebase-admin");
-require("dotenv").config();
+// const admin = require("firebase-admin");
+// require("dotenv").config();
 
-if (!process.env.FIREBASE_PRIVATE_KEY) {
-  throw new Error("FIREBASE_PRIVATE_KEY missing in .env");
-}
+// if (!process.env.FIREBASE_PRIVATE_KEY) {
+//   throw new Error("FIREBASE_PRIVATE_KEY missing in .env");
+// }
 
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.auth_uri,
-  token_uri: process.env.token_uri,
-  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
-};
+// const serviceAccount = {
+//   type: "service_account",
+//   project_id: process.env.FIREBASE_PROJECT_ID,
+//   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+//   private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+//   client_email: process.env.FIREBASE_CLIENT_EMAIL,
+//   client_id: process.env.FIREBASE_CLIENT_ID,
+//   auth_uri: process.env.auth_uri,
+//   token_uri: process.env.token_uri,
+//   auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+//   client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+// };
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
-module.exports = admin;
+// module.exports = admin;
