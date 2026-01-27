95a5dd033cc503d08c10c0d3d6cd970144c7372c
mukesh306 <mukesh@developmentcart.com>
2026-01-27 16:59:46 +0530

    update
    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -607,6 +607,7 @@ exports.loginWithOTP = async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: '7d',
     });
+    
 
     res.status(200).json({
       message: 'Login successful',
