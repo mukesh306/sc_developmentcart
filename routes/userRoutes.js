0b93f3e58a10f11a9e0a23e66dc8c40de015793a
mukesh306 <mukesh@developmentcart.com>
2026-02-26 16:36:46 +0530

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -121,6 +121,7 @@ router.get('/verify-token', (req, res) => {
 
 
 router.post("/Razorpay/create-payment", auth, userController.createRazorpayOrder);
+
 router.post("/Razorpay/verify-payment", auth, userController.verifyRazorpayPayment);
 
 
